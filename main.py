35fcfdcdf95a3edb83c4e87c841e607f0ffecef8
Carlos <3477186+CarlosLeInfant@users.noreply.github.com>
2025-06-14 09:41:14 +0200

    Refactor entrypoint

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+"""Example application entry point."""
+
+
+def main() -> None:
+    """Run the application."""
+    print("Hello, World!")
+
+
+if __name__ == "__main__":  # pragma: no cover - entry point
+    main()
